eb684c2c43f3d9dbd5a8cbd5356bf55b6ecb4733
Bojan Ludajic <bojan.ludajic@gmail.com>
2025-02-16 00:06:48 +0100

    refactor part service http client

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -2,14 +2,16 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public RestClient restClient() {
+		return RestClient.builder()
+				.baseUrl("http://localhost:8081/Parts/")
+				.build();
 	}
 
 }
--- src/main/java/com/example/demo/service/PartService.java
+++ src/main/java/com/example/demo/service/PartService.java
@@ -1,18 +1,12 @@
 package com.example.demo.service;
 
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import com.example.demo.dto.CarRequestDTO;
 import com.example.demo.dto.PartDTO;
@@ -20,50 +14,33 @@ import com.example.demo.dto.PartDTO;
 @Service
 @Lazy
 public class PartService {
-	
-	final RestTemplate restTemplate;
 
-	public PartService(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
+	final RestClient restClient;
+
+	public PartService(RestClient restClient) {
+        this.restClient = restClient;
 	}
 
-
 	public List<PartDTO> getParts(String make, String model) {
-		String url = "http://localhost:8081/Parts/partsForModel?make=" + make + "&model=" + model;
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+		String url = "/partsForModel?make=" + make + "&model=" + model;
 
-		HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(headers);
-		
-		ResponseEntity<List<PartDTO>> response = restTemplate.exchange(
-				url,
-				HttpMethod.GET,
-				requestEntity,
-				new ParameterizedTypeReference<List<PartDTO>>() {}
-		);
-		
-		return response.getBody();
+		return restClient.get()
+				.uri(url)
+				.retrieve()
+				.body(new ParameterizedTypeReference<>() {
+                });
 	}
 	
 	public ResponseEntity<String> orderPart(String make, String model, String name) {
-		String url = "http://localhost:8081/Parts/order";
-		
-		CarRequestDTO dto = new CarRequestDTO(make, model, name);
-		
-		HttpHeaders  headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		
-		HttpEntity<CarRequestDTO> requestEntity = new HttpEntity<>(dto, headers);
+		String url = "/order";
 
-		ResponseEntity<String> response = restTemplate.exchange(
-				url, 
-				HttpMethod.PUT,
-				requestEntity,
-				String.class
-		);
-				
-		return response;
+		CarRequestDTO dto = new CarRequestDTO(make, model, name);
+
+		return restClient.put()
+				.uri(url)
+				.body(dto)
+				.retrieve()
+				.toEntity(String.class);
 	}
 	
 
